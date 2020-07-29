983aa696e7e625568f872277b9c771c3a8bac694
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-07-29 22:21:49 +0500

    Меню + картинки

--- shlif-project/js/script.js
+++ shlif-project/js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
 
+    $('.header__burger').click(function () {
+        $('.header__burger').toggleClass('header__burger_active');
+        $('.header-nav__inner').toggleClass('header-nav__inner_active');
+        $('body').toggleClass('body_lock');
+    });
+
     $('.gallery__slider').slick({
         dots: true
     });
