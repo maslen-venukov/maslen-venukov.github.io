788feeff1c0bd8e3f5923e4929a2a88a9995a810
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-07-30 13:33:10 +0500

    Пояснения
    Комментарии в script.js + "Обратный звонок" в форме в подвале

--- shlif-project/js/script.js
+++ shlif-project/js/script.js
@@ -1,45 +1,45 @@
 $(document).ready(function () {
 
+    // Меню-бургер
     $('.header__burger').click(function () {
         $('.header__burger').toggleClass('header__burger_active');
         $('.header-nav__inner').toggleClass('header-nav__inner_active');
         $('body').toggleClass('body_lock');
     });
 
-    //E-mail Ajax Send
-    $(".contact__form").submit(function() { //Change
+    // Отправка формы на странице Контакты
+    $(".contact__form").submit(function() {
         var th = $(this);
         $.ajax({
             type: "POST",
-            url: "mail.php", //Change
+            url: "mail.php",
             data: th.serialize()
         }).done(function() {
             alert("Сообщение отправлено!");
             setTimeout(function() {
-                // Done Functions
                 th.trigger("reset");
             }, 1000);
         });
         return false;
     });
 
-    //E-mail Ajax Send
-    $(".footer-callback__form").submit(function() { //Change
+    // Отправка формы из подвала
+    $(".footer-callback__form").submit(function() {
         var th = $(this);
         $.ajax({
             type: "POST",
-            url: "mail.php", //Change
+            url: "mail.php",
             data: th.serialize()
         }).done(function() {
             alert("Сообщение отправлено!");
             setTimeout(function() {
-                // Done Functions
                 th.trigger("reset");
             }, 1000);
         });
         return false;
     });
 
+    // Слайдер на страницах Наши работы и Спортзалы
     $('.gallery__slider').slick({
         dots: true
     });
