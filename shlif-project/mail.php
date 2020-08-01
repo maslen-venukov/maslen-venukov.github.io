d9401e9c4af57b3fe0f47fcee7e3a0ce4915c453
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-08-01 16:04:07 +0500

    2 почты

--- shlif-project/mail.php
+++ shlif-project/mail.php
@@ -8,10 +8,11 @@ if ( $method === 'POST' ) {
 
 	$project_name = trim($_POST["project_name"]);
 	$admin_email  = trim($_POST["admin_email"]);
+	$admin_email  = trim($_POST["admin_email_2"]);
 	$form_subject = trim($_POST["form_subject"]);
 
 	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "admin_email_2" && $key != "form_subject" ) {
 			$message .= "
 			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
 				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
@@ -24,10 +25,11 @@ if ( $method === 'POST' ) {
 
 	$project_name = trim($_GET["project_name"]);
 	$admin_email  = trim($_GET["admin_email"]);
+	$admin_email  = trim($_POST["admin_email_2"]);
 	$form_subject = trim($_GET["form_subject"]);
 
 	foreach ( $_GET as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "admin_email_2" && $key != "form_subject" ) {
 			$message .= "
 			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
 				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
@@ -50,3 +52,10 @@ $headers = "MIME-Version: 1.0" . PHP_EOL .
 'Reply-To: '.$admin_email.'' . PHP_EOL;
 
 mail($admin_email, adopt($form_subject), $message, $headers );
+
+$headers_2 = "MIME-Version: 1.0" . PHP_EOL .
+"Content-Type: text/html; charset=utf-8" . PHP_EOL .
+'From: '.adopt($project_name).' <'.$admin_email_2.'>' . PHP_EOL .
+'Reply-To: '.$admin_email_2.'' . PHP_EOL;
+
+mail($admin_email_2, adopt($form_subject), $message, $headers_2 );
