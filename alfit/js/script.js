80b608474493577e58917e83dc6e8db1b929d41c
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-08-06 00:24:29 +0500

    Футер

--- alfit/js/script.js
+++ alfit/js/script.js
@@ -2,4 +2,32 @@ $(document).ready(function () {
 
     $('.js-timeline').Timeline();
 
+    $('.reviews__row').slick({
+        variableWidth: true,
+        infinite: false,
+        touchThreshold: 100
+    });
+
+    $('.reviews-arrows__left').click(function () { 
+        $('.reviews .slick-prev').click();
+    });
+
+    $('.reviews-arrows__right').click(function () { 
+        $('.reviews .slick-next').click();
+    });
+
+    $('.production__row').slick({
+        variableWidth: true,
+        infinite: false,
+        touchThreshold: 100
+    });
+
+    $('.production-arrows__left').click(function () { 
+        $('.production .slick-prev').click();
+    });
+
+    $('.production-arrows__right').click(function () { 
+        $('.production .slick-next').click();
+    });
+
 });
