9b8603285c40cba7d5a0a9925f39301323beda2e
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-08-06 22:08:37 +0500

    Анимация + фавикон

--- alfit/js/script.js
+++ alfit/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    // Timeline / История АЛФИТ
+
     $('.js-timeline').Timeline();
 
     function checkTimeline() {
@@ -11,6 +13,8 @@ $(document).ready(function () {
     $(window).resize(function() {
         checkTimeline();
     });
+
+    // Slick Slider / Отзывы
 
     $('.reviews__row').slick({
         variableWidth: true,
@@ -27,6 +31,8 @@ $(document).ready(function () {
         ]
     });
 
+    // Slick Slider / Отзывы / Стрелки
+
     $('.reviews-arrows__left').click(function () { 
         $('.reviews .slick-prev').click();
     });
@@ -34,6 +40,8 @@ $(document).ready(function () {
     $('.reviews-arrows__right').click(function () { 
         $('.reviews .slick-next').click();
     });
+
+    // Slick Slider / Собственное производство
 
     $('.production__row').slick({
         variableWidth: true,
@@ -50,6 +58,8 @@ $(document).ready(function () {
         ]
     });
 
+    // Slick Slider / Собственное производство / Стрелки
+
     $('.production-arrows__left').click(function () { 
         $('.production .slick-prev').click();
     });
@@ -58,19 +68,29 @@ $(document).ready(function () {
         $('.production .slick-next').click();
     });
 
+    // Бургер меню
+
     $('.nav__burger').click(function () {
         $(this).toggleClass('nav__burger_active');
         $('.nav__inner').toggleClass('nav__inner_active');
     });
+
+    // Модальные окна
 
     $('.modal__close, .modal-online__close').click(function() {
         $('.modal__outer').removeClass('modal__outer_active');
         $('.modal-online__outer').removeClass('modal-online__outer_active');
     });
 
+    // Правовая информация / Пользовательское соглашение
+
     $('.rights__title, .agreement__title').click(function() {
         $('.rights').removeClass('rights_active');
         $('.agreement').removeClass('agreement_active');
     });
+
+    // AOS / Анимация
+
+    AOS.init();
 
 });
