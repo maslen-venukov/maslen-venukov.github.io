02617a6208734c6f993e97097be6d6db431bf43d
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-08-08 16:21:42 +0500

    Пол адаптива

--- fitcurves/js/script.js
+++ fitcurves/js/script.js
@@ -5,15 +5,33 @@ $(document).ready(function () {
         centerPadding: '0px',
         slidesToShow: 3,
         initialSlide: 1,
+        responsive: [
+            {
+                breakpoint: 901,
+                settings: {
+                    slidesToShow: 1,
+                    adaptiveHeight: true
+                }
+            }
+        ]
     });
 
     $('.result__slider').slick({
         slidesToShow: 6,
+        responsive: [
+            {
+                breakpoint: 581,
+                settings: {
+                    slidesToShow: 3,
+                }
+            }
+        ]
     });
 
     $('.reviews__slider').slick({
         variableWidth: true,
-        dots: true
+        dots: true,
+        infinite: false
     });
 
     $('.gyms__slider').slick({
@@ -21,6 +39,19 @@ $(document).ready(function () {
         dots: true,
         centerMode: true,
         centerPadding: '0px',
+    });
+
+    $('.first__slider').slick({
+        variableWidth: true,
+        centerMode: true,
+        centerPadding: '0px',
+        initialSlide: 1,
+        infinite: false
+    });
+
+    $('.nav__burger').click(function () {
+        $(this).toggleClass('nav__burger_active');
+        $('.nav__inner').toggleClass('nav__inner_active');
     });
 
 });
