82a89dc48bc8d9814bce48210121d5318e481d2c
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-08-10 16:47:22 +0500

    Слайдер в блоге

--- agro/js/script.js
+++ agro/js/script.js
@@ -2,7 +2,32 @@ $(document).ready(function () {
 
     $('.blog__slider').slick({
         variableWidth: true,
-        infinite: false
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 611,
+                settings: {
+                    variableWidth: false
+                }
+            }
+        ]
+    });
+
+    function blogCounter() {
+        const total = $('.blog-slider__item').length;
+        $('.blog__number-total').text(total);
+        const current = $('.blog-slider__item').index($('.blog-slider__item.slick-current.slick-active'))+1;
+        $('.blog__number-current').text(current);
+    } blogCounter();
+
+    $('.blog__arrow-left').click(function () {
+        $('.blog__slider .slick-prev').click();
+        blogCounter();
+    });
+
+    $('.blog__arrow-right').click(function () {
+        $('.blog__slider .slick-next').click();
+        blogCounter();
     });
 
     $('.production__slider').slick({
@@ -18,21 +43,21 @@ $(document).ready(function () {
         ]
     });
 
-    function counter() {
+    function productionCounter() {
         const total = $('.production-slider__item').length;
         $('.production__number-total').text(total);
         const current = $('.production-slider__item').index($('.production-slider__item.slick-current.slick-active'))+1;
         $('.production__number-current').text(current);
-    } counter();
+    } productionCounter();
 
     $('.production__arrow-left').click(function () {
-        $('.slick-prev').click();
-        counter();
+        $('.production__slider .slick-prev').click();
+        productionCounter();
     });
 
     $('.production__arrow-right').click(function () {
-        $('.slick-next').click();
-        counter();
+        $('.production__slider .slick-next').click();
+        productionCounter();
     });
 
     $('.nav__burger').click(function () {
