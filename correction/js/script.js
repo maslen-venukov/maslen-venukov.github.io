05925d36778addc361075c37853ac9bcb2a3423c
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-08-26 16:13:49 +0500

    Часть адаптива

--- correction/js/script.js
+++ correction/js/script.js
@@ -1,9 +1,35 @@
 $(document).ready(function () {
 
+    // Слайдер в отзывах
+
     $('.reviews__slider').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1131,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 751,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    adaptiveHeight: true
+                }
+            }
+        ]
+    });
+
+    // Меню в шапке
+
+    $('.header__burger').click(function () {
+        $(this).toggleClass('header__burger--active');
+        $('.nav--mobile').toggleClass('nav--mobile--active');
     });
 
 });
