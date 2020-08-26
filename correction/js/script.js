fbdfa2ee58775772635a396eabca54ecad688d61
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-08-26 18:08:44 +0500

    Конец адаптива

--- correction/js/script.js
+++ correction/js/script.js
@@ -30,6 +30,7 @@ $(document).ready(function () {
     $('.header__burger').click(function () {
         $(this).toggleClass('header__burger--active');
         $('.nav--mobile').toggleClass('nav--mobile--active');
+        $('body, html').toggleClass('lock');
     });
 
 });
