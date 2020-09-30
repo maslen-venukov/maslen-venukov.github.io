8186cd91effe0da4ccaa0ad40ff996777fba42a6
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-09-30 18:03:49 +0500

    Адаптив без футера

--- tikmusic/js/script.js
+++ tikmusic/js/script.js
@@ -1,6 +1,9 @@
 const cookies = document.querySelector('.cookies');
 const cookiesBtn = document.querySelector('.cookies__btn');
 const navLinkFunctional = document.querySelector('.nav__link[href="#functional"');
+const introScroll = document.querySelector('.intro__scroll');
+const headerBurger = document.querySelector('.header__burger');
+const headerNavList = document.querySelector('.header__nav .nav__list');
 
 setTimeout(() => {
   cookies.classList.add('cookies--active');
@@ -18,7 +21,23 @@ const scrollToId = () => {
   });
 };
 
-navLinkFunctional.addEventListener('click', (e) => {
+navLinkFunctional.addEventListener('click', e => {
   e.preventDefault();
   scrollToId();
+  headerBurger.classList.remove('header__burger--active');
+  headerNavList.classList.remove('nav__list--active');
+  document.documentElement.classList.remove('lock');
+  document.body.classList.remove('lock');
+});
+
+introScroll.addEventListener('click', e => {
+  e.preventDefault();
+  scrollToId();
+});
+
+headerBurger.addEventListener('click', () => {
+  headerBurger.classList.toggle('header__burger--active');
+  headerNavList.classList.toggle('nav__list--active');
+  document.documentElement.classList.toggle('lock');
+  document.body.classList.toggle('lock');
 });
