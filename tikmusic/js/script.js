42cb2d5ee98ea181b9e2eeb8407398a1216542d2
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-09-30 19:19:23 +0500

    Правки

--- tikmusic/js/script.js
+++ tikmusic/js/script.js
@@ -4,14 +4,19 @@ const navLinkFunctional = document.querySelector('.nav__link[href="#functional"'
 const introScroll = document.querySelector('.intro__scroll');
 const headerBurger = document.querySelector('.header__burger');
 const headerNavList = document.querySelector('.header__nav .nav__list');
+const tabsHeader = document.querySelector('.tabs__header');
 
-setTimeout(() => {
-  cookies.classList.add('cookies--active');
-}, 500);
+if(cookies) {
 
-cookiesBtn.addEventListener('click', () => {
-  cookies.classList.remove('cookies--active');
-});
+  setTimeout(() => {
+    cookies.classList.add('cookies--active');
+  }, 500);
+
+  cookiesBtn.addEventListener('click', () => {
+    cookies.classList.remove('cookies--active');
+  });
+
+}
 
 const scrollToId = () => {
   const section = document.querySelector(navLinkFunctional.getAttribute('href'));
@@ -21,23 +26,39 @@ const scrollToId = () => {
   });
 };
 
-navLinkFunctional.addEventListener('click', e => {
-  e.preventDefault();
-  scrollToId();
-  headerBurger.classList.remove('header__burger--active');
-  headerNavList.classList.remove('nav__list--active');
-  document.documentElement.classList.remove('lock');
-  document.body.classList.remove('lock');
-});
+if(navLinkFunctional) {
+  navLinkFunctional.addEventListener('click', e => {
+    e.preventDefault();
+    scrollToId();
+    headerBurger.classList.remove('header__burger--active');
+    headerNavList.classList.remove('nav__list--active');
+    document.documentElement.classList.remove('lock');
+    document.body.classList.remove('lock');
+  });
+};
 
-introScroll.addEventListener('click', e => {
-  e.preventDefault();
-  scrollToId();
-});
+if(introScroll) {
+  introScroll.addEventListener('click', e => {
+    e.preventDefault();
+    scrollToId();
+  });
+};
 
-headerBurger.addEventListener('click', () => {
-  headerBurger.classList.toggle('header__burger--active');
-  headerNavList.classList.toggle('nav__list--active');
-  document.documentElement.classList.toggle('lock');
-  document.body.classList.toggle('lock');
-});
+if(headerBurger) {
+  headerBurger.addEventListener('click', () => {
+    headerBurger.classList.toggle('header__burger--active');
+    headerNavList.classList.toggle('nav__list--active');
+    document.documentElement.classList.toggle('lock');
+    document.body.classList.toggle('lock');
+  });
+};
+
+if(tabsHeader) {
+  tabsHeader.querySelector('.tabs__link').textContent = document.querySelector('.tabs__list .tabs__item--active .tabs__link').textContent;
+  console.log(document.querySelector('.tabs__list .tabs__item--active .tabs__link').textContent);
+
+  tabsHeader.addEventListener('click', () => {
+    tabsHeader.classList.toggle('tabs__header--active');
+  });
+
+};
