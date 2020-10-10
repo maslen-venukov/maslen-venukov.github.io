e5b0659d8b2b0830c32df8ece538e68a8fe209aa
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-10-10 22:46:36 +0500

    adaptive w/o intro

--- anson-baer/js/script.js
+++ anson-baer/js/script.js
@@ -1,5 +1,9 @@
 const circlesItems = document.querySelectorAll('.circles__item');
 const headersItems = document.querySelectorAll('.headers__item');
+const htmlBody = document.querySelector('html, body');
+const headerBurger = document.querySelector('.header__burger');
+const headerSocial = document.querySelector('.header__social');
+const navList = document.querySelector('.nav__list');
 
 circlesItems.forEach((el, index) => {
   el.addEventListener('mouseover', e => {
@@ -10,4 +14,10 @@ circlesItems.forEach((el, index) => {
       headersItems[index].style.display = 'block';
     };
   });
+});
+
+headerBurger.addEventListener('click', () => {
+  htmlBody.classList.toggle('lock');
+  headerSocial.classList.toggle('header__social--active');
+  navList.classList.toggle('nav__list--active');
 });
