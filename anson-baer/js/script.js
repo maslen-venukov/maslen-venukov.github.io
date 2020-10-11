2542920130cd6e4fe68458fb5abb2c02cb5e337b
maslen-venukov <67370685+maslen-venukov@users.noreply.github.com>
2020-10-11 14:16:56 +0500

    main screen adaptive

--- anson-baer/js/script.js
+++ anson-baer/js/script.js
@@ -6,6 +6,19 @@ const headerSearch = document.querySelector('.header__search');
 const headerSocial = document.querySelector('.header__social');
 const navList = document.querySelector('.nav__list');
 
+// burger menu
+
+headerBurger.addEventListener('click', () => {
+  htmlBody.classList.toggle('lock');
+  headerSocial.classList.toggle('header__social--active');
+  setTimeout(() => {
+    headerSearch.classList.toggle('header__search--active');
+  }, 300);
+  navList.classList.toggle('nav__list--active');
+});
+
+// circles on the main screen
+
 circlesItems.forEach((el, index) => {
   el.addEventListener('mouseover', e => {
     if(e.target.classList.contains('circles__item')) {
@@ -17,11 +30,27 @@ circlesItems.forEach((el, index) => {
   });
 });
 
-headerBurger.addEventListener('click', () => {
-  htmlBody.classList.toggle('lock');
-  headerSocial.classList.toggle('header__social--active');
-  setTimeout(() => {
-    headerSearch.classList.toggle('header__search--active');
-  }, 300);
-  navList.classList.toggle('nav__list--active');
+// circles slider on the main screen
+
+const checkWindowWidth = () => {
+  if(window.innerWidth <= 1200) {
+    circlesItems.forEach(el => {
+      el.classList.add('swiper-slide');
+    });
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 15,
+      centeredSlides: true,
+    });
+  } else {
+    circlesItems.forEach(el => {
+      el.classList.remove('swiper-slide');
+    });
+  };
+};
+
+checkWindowWidth();
+
+window.addEventListener('resize', () => {
+  checkWindowWidth();
 });
